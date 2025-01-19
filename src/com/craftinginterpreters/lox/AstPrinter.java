b4bda822c90217004f0637ed3775a8645ab68b77
Damilola Ajiboye <ajibigad@gmail.com>
2025-01-19 07:11:16 +0000

    chapter 9 changes

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -2,9 +2,64 @@ package com.craftinginterpreters.lox;
 
 import java.util.List;
 
-class AstPrinter implements Expr.Visitor<String> {
+class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
   String print(Expr expr) {
     return expr.accept(this);
+  }
+
+  String print(Stmt stmt) {
+    return stmt.accept(this);
+  }
+  @Override
+  public String visitBlockStmt(Stmt.Block stmt) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("(block ");
+
+    for (Stmt statement : stmt.statements) {
+      builder.append(statement.accept(this));
+    }
+
+    builder.append(")");
+    return builder.toString();
+  }
+
+  @Override
+  public String visitExpressionStmt(Stmt.Expression stmt) {
+    return parenthesize(";", stmt.expression);
+  }
+
+  @Override
+  public String visitIfStmt(Stmt.If stmt) {
+    if (stmt.elseBranch == null) {
+      return parenthesize2("if", stmt.condition, stmt.thenBranch);
+    }
+
+    return parenthesize2("if-else", stmt.condition, stmt.thenBranch,
+        stmt.elseBranch);
+  }
+
+  @Override
+  public String visitPrintStmt(Stmt.Print stmt) {
+    return parenthesize("print", stmt.expression);
+  }
+
+  @Override
+  public String visitVarStmt(Stmt.Var stmt) {
+    if (stmt.initializer == null) {
+      return parenthesize2("var", stmt.name);
+    }
+
+    return parenthesize2("var", stmt.name, "=", stmt.initializer);
+  }
+
+  @Override
+  public String visitWhileStmt(Stmt.While stmt) {
+    return parenthesize2("while", stmt.condition, stmt.body);
+  }
+
+  @Override
+  public String visitAssignExpr(Expr.Assign expr) {
+    return parenthesize2("=", expr.name.lexeme, expr.value);
   }
 
   @Override
@@ -25,8 +80,18 @@ class AstPrinter implements Expr.Visitor<String> {
   }
 
   @Override
+  public String visitLogicalExpr(Expr.Logical expr) {
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  @Override
   public String visitUnaryExpr(Expr.Unary expr) {
     return parenthesize(expr.operator.lexeme, expr.right);
+  }
+
+  @Override
+  public String visitVariableExpr(Expr.Variable expr) {
+    return expr.name.lexeme;
   }
   private String parenthesize(String name, Expr... exprs) {
     StringBuilder builder = new StringBuilder();
@@ -58,6 +123,8 @@ class AstPrinter implements Expr.Visitor<String> {
       builder.append(" ");
       if (part instanceof Expr) {
         builder.append(((Expr)part).accept(this));
+      } else if (part instanceof Stmt) {
+        builder.append(((Stmt) part).accept(this));
       } else if (part instanceof Token) {
         builder.append(((Token) part).lexeme);
       } else if (part instanceof List) {
--- src/com/craftinginterpreters/lox/Expr.java
+++ src/com/craftinginterpreters/lox/Expr.java
@@ -5,13 +5,32 @@ import java.util.List;
 
 abstract class Expr {
   interface Visitor<R> {
+    R visitAssignExpr(Assign expr);
     R visitBinaryExpr(Binary expr);
     R visitGroupingExpr(Grouping expr);
     R visitLiteralExpr(Literal expr);
+    R visitLogicalExpr(Logical expr);
     R visitUnaryExpr(Unary expr);
+    R visitVariableExpr(Variable expr);
   }
 
   // Nested Expr classes here...
+//> expr-assign
+  static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+      this.name = name;
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitAssignExpr(this);
+    }
+
+    final Token name;
+    final Expr value;
+  }
+//< expr-assign
 //> expr-binary
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
@@ -58,6 +77,24 @@ abstract class Expr {
     final Object value;
   }
 //< expr-literal
+//> expr-logical
+  static class Logical extends Expr {
+    Logical(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLogicalExpr(this);
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+//< expr-logical
 //> expr-unary
   static class Unary extends Expr {
     Unary(Token operator, Expr right) {
@@ -74,6 +111,20 @@ abstract class Expr {
     final Expr right;
   }
 //< expr-unary
+//> expr-variable
+  static class Variable extends Expr {
+    Variable(Token name) {
+      this.name = name;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVariableExpr(this);
+    }
+
+    final Token name;
+  }
+//< expr-variable
 
   abstract <R> R accept(Visitor<R> visitor);
 }
