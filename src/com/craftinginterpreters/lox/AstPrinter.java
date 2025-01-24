618ada82c457ed9917b045a8f716759bc5eb4d4c
Damilola Ajiboye <ajibigad@gmail.com>
2025-01-24 04:25:49 +0000

    chapter 10 changes

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -2,15 +2,101 @@ package com.craftinginterpreters.lox;
 
 import java.util.List;
 
-class AstPrinter implements Expr.Visitor<String> {
+class AstPrinter implements Expr.Visitor<String>, Stmt.Visitor<String> {
   String print(Expr expr) {
     return expr.accept(this);
+  }
+
+  String print(Stmt stmt) {
+    return stmt.accept(this);
+  }
+  @Override
+  public String visitBlockStmt(Stmt.Block stmt) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("(block ");
+
+    for (Stmt statement : stmt.statements) {
+      builder.append(statement.accept(this));
+    }
+
+    builder.append(")");
+    return builder.toString();
+  }
+
+  @Override
+  public String visitExpressionStmt(Stmt.Expression stmt) {
+    return parenthesize(";", stmt.expression);
+  }
+
+  @Override
+  public String visitFunctionStmt(Stmt.Function stmt) {
+    StringBuilder builder = new StringBuilder();
+    builder.append("(fun " + stmt.name.lexeme + "(");
+
+    for (Token param : stmt.params) {
+      if (param != stmt.params.get(0)) builder.append(" ");
+      builder.append(param.lexeme);
+    }
+
+    builder.append(") ");
+
+    for (Stmt body : stmt.body) {
+      builder.append(body.accept(this));
+    }
+
+    builder.append(")");
+    return builder.toString();
+  }
+
+  @Override
+  public String visitIfStmt(Stmt.If stmt) {
+    if (stmt.elseBranch == null) {
+      return parenthesize2("if", stmt.condition, stmt.thenBranch);
+    }
+
+    return parenthesize2("if-else", stmt.condition, stmt.thenBranch,
+        stmt.elseBranch);
+  }
+
+  @Override
+  public String visitPrintStmt(Stmt.Print stmt) {
+    return parenthesize("print", stmt.expression);
+  }
+
+  @Override
+  public String visitReturnStmt(Stmt.Return stmt) {
+    if (stmt.value == null) return "(return)";
+    return parenthesize("return", stmt.value);
+  }
+
+  @Override
+  public String visitVarStmt(Stmt.Var stmt) {
+    if (stmt.initializer == null) {
+      return parenthesize2("var", stmt.name);
+    }
+
+    return parenthesize2("var", stmt.name, "=", stmt.initializer);
+  }
+
+  @Override
+  public String visitWhileStmt(Stmt.While stmt) {
+    return parenthesize2("while", stmt.condition, stmt.body);
+  }
+
+  @Override
+  public String visitAssignExpr(Expr.Assign expr) {
+    return parenthesize2("=", expr.name.lexeme, expr.value);
   }
 
   @Override
   public String visitBinaryExpr(Expr.Binary expr) {
     return parenthesize(expr.operator.lexeme,
                         expr.left, expr.right);
+  }
+
+  @Override
+  public String visitCallExpr(Expr.Call expr) {
+    return parenthesize2("call", expr.callee, expr.arguments);
   }
 
   @Override
@@ -25,8 +111,18 @@ class AstPrinter implements Expr.Visitor<String> {
   }
 
   @Override
+  public String visitLogicalExpr(Expr.Logical expr) {
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  @Override
   public String visitUnaryExpr(Expr.Unary expr) {
     return parenthesize(expr.operator.lexeme, expr.right);
+  }
+
+  @Override
+  public String visitVariableExpr(Expr.Variable expr) {
+    return expr.name.lexeme;
   }
   private String parenthesize(String name, Expr... exprs) {
     StringBuilder builder = new StringBuilder();
@@ -58,6 +154,8 @@ class AstPrinter implements Expr.Visitor<String> {
       builder.append(" ");
       if (part instanceof Expr) {
         builder.append(((Expr)part).accept(this));
+      } else if (part instanceof Stmt) {
+        builder.append(((Stmt) part).accept(this));
       } else if (part instanceof Token) {
         builder.append(((Token) part).lexeme);
       } else if (part instanceof List) {
